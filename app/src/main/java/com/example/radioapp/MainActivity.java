c1dd10c1f261ed272aa6e6cccb8df475d6e47e15
JinYo <zsxtoucan@gmail.com>
2020-03-30 10:42:23 +0800

    0330

--- app/src/main/java/com/example/radioapp/MainActivity.java
+++ app/src/main/java/com/example/radioapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.radioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void btnOK(View view){
+        TextView txvLike = findViewById(R.id.textView2);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+
+        int id = radioGroup.getCheckedRadioButtonId();
+        RadioButton seleted = findViewById(id);
+
+        txvLike.setText("喜歡的英文："+seleted.getText());
     }
 }
